a263ba94ff372e7cd64e7e322ef2dc4717656beb
Vincent <vincent.hays@nielseniq.com>
2025-01-14 23:35:47 +0100

    part 1 day 1

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -1,6 +1,5 @@
 fn main() {
-    let res: u32 =
-        include_str!("input.txt")
+    let res: u32 = include_str!("input.txt")
         .lines()
         .into_iter()
         .map(|l| {
@@ -10,14 +9,24 @@ fn main() {
                 .map(|l| l.parse().unwrap())
                 .collect::<Vec<u32>>();
 
-            let is_level_inc = levels.windows(2).into_iter().all(|l| l[0] < l[1] && l[1] <= l[0]+3);
-            let is_level_dec = levels.windows(2).into_iter().all(|l| l[0] > l[1] && l[0] <= l[1]+3);
+            let is_level_inc = levels
+                .windows(2)
+                .into_iter()
+                .all(|l| l[0] < l[1] && l[1] <= l[0] + 3);
+            let is_level_dec = levels
+                .windows(2)
+                .into_iter()
+                .all(|l| l[0] > l[1] && l[0] <= l[1] + 3);
 
             println!("levels: {:?}", levels);
             println!("is_level_inc: {}", is_level_inc);
             println!("is_level_dec: {}", is_level_dec);
 
-            if is_level_inc || is_level_dec { 1 } else { 0 }
+            if is_level_inc || is_level_dec {
+                1
+            } else {
+                0
+            }
         })
         .sum();
 
