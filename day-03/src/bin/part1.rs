7f4862130ccbedfa339e585cac7ac0c616d360fe
Vincent <vincent.hays@nielseniq.com>
2025-01-15 12:16:19 +0100

    part 2 day 3

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -1,19 +1,18 @@
 use regex::Regex;
 
 fn main() {
+    let data = include_str!("input.txt");
     let regex = Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)").unwrap();
-    let res = include_str!("input.txt")
-        .lines()
-        .into_iter()
-        .map(|line| regex
-            .captures_iter(line)
-            .map(|c| {
-                println!("{c:?}");
-                let a: u64 = c.name("a").unwrap().as_str().parse().unwrap();
-                let b: u64 = c.name("b").unwrap().as_str().parse().unwrap();
-                a * b
-            }).sum::<u64>()
-        ).sum::<u64>();
+
+    let res = regex
+        .captures_iter(data)
+        .map(|c| {
+            println!("{c:?}");
+            let a: u64 = c.name("a").unwrap().as_str().parse().unwrap();
+            let b: u64 = c.name("b").unwrap().as_str().parse().unwrap();
+            a * b
+        })
+        .sum::<u64>();
 
     println!("{}", res);
 }
