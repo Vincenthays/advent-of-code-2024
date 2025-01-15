dda1fbe20d0e137177c576a0a32749cf085bd3b0
Vincent <vincent.hays@nielseniq.com>
2025-01-15 11:49:04 +0100

    part 2 day 3

--- day-03/src/bin/part2.rs
+++ day-03/src/bin/part2.rs
@@ -1,23 +1,22 @@
 use regex::Regex;
 
 fn main() {
-    let regex = Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)|do\(\)|don't\(\)").unwrap();
     let data = include_str!("input.txt");
+    let regex = Regex::new(r"mul\((?<a>\d+),(?<b>\d+)\)|do\(\)|don't\(\)").unwrap();
 
     let mut sum = 0;
-    let mut active = true;
+    let mut is_active = true;
 
     for c in regex.captures_iter(data) {
-        match c.get(0).unwrap().as_str() {
-            "do()" => active = true,
-            "don't()" => active = false,
-            _ => {
-                if active {
-                    let a: u64 = c.name("a").unwrap().as_str().parse().unwrap();
-                    let b: u64 = c.name("b").unwrap().as_str().parse().unwrap();
-                    sum += a * b;
-                }
+        match (c.get(0).unwrap().as_str(), is_active) {
+            ("do()", _) => is_active = true,
+            ("don't()", _) => is_active = false,
+            (_, true) => {
+                let a: u64 = c.name("a").unwrap().as_str().parse().unwrap();
+                let b: u64 = c.name("b").unwrap().as_str().parse().unwrap();
+                sum += a * b;
             }
+            _ => {}
         }
     }
 
