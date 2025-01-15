c89bd29b9ceec2da343172725c08e5e2988cfd3f
Vincent <vincent.hays@nielseniq.com>
2025-01-15 15:30:19 +0100

    part 2 day 3

--- day-02/src/bin/part2.rs
+++ day-02/src/bin/part2.rs
@@ -1,25 +1,39 @@
 fn main() {
-    let res: u32 =
-        include_str!("input.txt")
-            .lines()
-            .into_iter()
-            .map(|l| {
-                let levels = l
-                    .split_whitespace()
-                    .into_iter()
-                    .map(|l| l.parse().unwrap())
-                    .collect::<Vec<u32>>();
+    let res: u32 = include_str!("input.txt")
+        .lines()
+        .into_iter()
+        .map(|l| {
+            let levels = l
+                .split_whitespace()
+                .into_iter()
+                .map(|l| l.parse().unwrap())
+                .collect::<Vec<u32>>();
 
-                let is_level_inc = levels.windows(2).into_iter().all(|l| l[0] < l[1] && l[1] <= l[0]+3);
-                let is_level_dec = levels.windows(2).into_iter().all(|l| l[0] > l[1] && l[0] <= l[1]+3);
+            let level_inc = levels
+                .iter()
+                .zip(levels.iter().skip(1))
+                .filter_map(|(&a, &b)| match a < b && b <= a + 3 {
+                    true => Some(a),
+                    false => None,
+                })
+                .collect::<Vec<_>>();
 
-                println!("levels: {:?}", levels);
-                println!("is_level_inc: {}", is_level_inc);
-                println!("is_level_dec: {}", is_level_dec);
+            let level_dec= levels
+                .iter()
+                .zip(levels.iter().skip(1))
+                .filter_map(|(&a, &b)| match b < a && a <= b + 3 {
+                    true => Some(a),
+                    false => None,
+                })
+                .collect::<Vec<_>>();
 
-                if is_level_inc || is_level_dec { 1 } else { 0 }
-            })
-            .sum();
+            println!("levels: {:?}", levels);
+            println!("is_level_inc: {:?}", level_inc);
+            println!("is_level_inc: {:?}", level_dec);
+
+            0
+        })
+        .sum();
 
     println!("{:?}", res);
 }
