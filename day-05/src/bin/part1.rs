f5dc44459fadc3862ceba8dafa97bf0b164fbc45
Vincent <vincent.hays@nielseniq.com>
2025-01-15 20:26:38 +0100

    part 2 day 3

---
+++ day-05/src/bin/part1.rs
@@ -0,0 +1,42 @@
+use std::ops::Index;
+
+fn main() {
+    let (order, manuals) = include_str!("input.txt").split_once("\n\n").unwrap();
+    let order = order
+        .lines()
+        .map(|l| {
+            let (a, b) = l.split_once('|').unwrap();
+            let a = a.parse::<u32>().unwrap();
+            let b = b.parse::<u32>().unwrap();
+            (a, b)
+        })
+        .collect::<Vec<_>>();
+
+    let res = manuals
+        .lines()
+        .map(|l| {
+            let manual = l.split(',')
+                .into_iter()
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect::<Vec<_>>();
+
+            for &(a, b) in order.iter() {
+                let Some(i) = manual.iter().position(|&x| x == a) else {
+                    continue
+                };
+                let Some(j) = manual.iter().position(|&x| x == b) else {
+                    continue
+                };
+                if i > j {
+                    return 0;
+                }
+            }
+
+            println!("{:?} - {:?}", &manual, manual[manual.len()/2]);
+
+            manual[manual.len()/2]
+        })
+        .sum::<u32>();
+
+    println!("res: {res}");
+}
